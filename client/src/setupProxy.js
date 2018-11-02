bce37d292fc4c815e72ec2e262bfbd1439b1ed02
nafidurmus <nafidurmus07@gmail.com>
2018-11-02 16:36:57 +0300

    for deploying #10

---
+++ client/src/setupProxy.js
@@ -0,0 +1,5 @@
+// const proxy = require('http-proxy-middleware');
+// module.exports = function(app) {
+//   console.log("exporting");
+//   app.use(proxy("/api", { target: "http://localhost:3001/", ws:true} ));
+// }
