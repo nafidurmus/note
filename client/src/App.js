aaadd8726e5c11d65ffdba3fa8bb1a586420e909
nafidurmus <nafidurmus07@gmail.com>
2018-11-02 15:51:49 +0300

    Update welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hello Hiplyst!</h1>
-        </header>
+       
         <ListsContainer />
       </div>
     );
